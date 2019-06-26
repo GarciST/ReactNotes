f87089c62e7c32596e74ef407624f8e483b07ddc
Garcia Diaz, S <s.garcia.diaz@accenture.com>
2019-06-26 22:08:30 +0200

    New: React Elements; Login development.

--- src/pods/login/login.component.tsx
+++ src/pods/login/login.component.tsx
@@ -1,25 +1,54 @@
 import * as React from 'react';
-import { View, TextInput, Button } from 'react-native';
+import { View } from 'react-native';
+import { Button, Input, Text, ThemeProvider } from 'react-native-elements';
+import { Icon } from 'react-native-elements'
 
 interface Props {
 
+}
+
+const theme = {
+    Input: {
+        maxWidth: 100
+    }
 }
 
 export const LoginComponent = (props: Props) => {
     return (
         <View>
             <View>
-        
+
             </View>
             <View>
-                <TextInput 
-                textContentType="username"
-                placeholder="Username" />
-                <TextInput 
-                textContentType="password"
-                secureTextEntry={true}
-                placeholder="Password" />
-                <Button title="Sign in" onPress={_ => {}}/>
+                <Input
+                    textContentType="username"
+                    placeholder="Username"
+                    leftIcon={
+                        <Icon
+                            name='person'
+                            type='material'
+                            size={24}
+                            color='black'
+                        />
+                    } />
+                <Input
+                    textContentType="password"
+                    secureTextEntry={true}
+                    placeholder="Password"
+                    leftIcon={
+                        <Icon
+                            name='lock'
+                            type='material'
+                            size={24}
+                            color='black'
+                        />
+                    } />
+                <Button
+                    title="Sign in"
+                    onPress={_ => { }} />
+                <Text
+                    
+                >Create an account</Text>
             </View>
         </View>
     );
