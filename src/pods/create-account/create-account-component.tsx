7d8b1d11a7853784847150d9cad50f0db7ca699f
GarciST <sgd382@gmail.com>
2019-07-07 13:37:03 +0200

    lang starts

--- src/pods/create-account/create-account-component.tsx
+++ src/pods/create-account/create-account-component.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import { Input, Button } from 'react-native-elements';
 
 export const CreateAccountComponent = () => {
