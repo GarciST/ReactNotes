2958b4ef9f5125bde357d55aa53435c49e14b8a8
GarciST <sgd382@gmail.com>
2019-07-14 02:01:56 +0200

    working on create account

--- src/pods/create-account/components/create-account-form.component.tsx
+++ src/pods/create-account/components/create-account-form.component.tsx
@@ -8,42 +8,78 @@ interface IProps {
     buttonStyle?: ViewStyle
 }
 
-
+const onNextField = (input: React.RefObject<Input>): void => {
+    if (input && input.current) input.current.focus()
+}
 
 export const CreateAccountForm = (props: IProps): JSX.Element => {
 
-    const [username, setUsername] = React.useState();
-    const [email, setEmail] = React.useState();
-    const [password, setPassword] = React.useState();
-    const [confirmPassword, setConfirmPassword] = React.useState();
+    const username = React.useRef<Input>(null);
+    const email = React.useRef<Input>(null);
+    const password = React.useRef<Input>(null);
+    const confirmPassword = React.useRef<Input>(null);
+    const name = React.useRef<Input>(null);
+    const lastname = React.useRef<Input>(null);
+
 
     return (
         <View style={props.trailerStyle}>
             <Input
+                ref={username}
                 placeholder="Username"
+                textContentType="username"
+                returnKeyType="next"
+                autoFocus={true}
                 leftIcon={{ type: "material", name: "person" }}
                 style={props.inputStyle}
-                onSubmitEditing={_ => email.focus()} />
+                errorStyle={{ color: 'red' }}
+                errorMessage='ENTER A VALID ERROR HERE'
+                onSubmitEditing={_ => onNextField(email)} />
             <Input
                 ref={email}
                 placeholder="Email"
+                keyboardType="email-address"
+                textContentType="emailAddress"
+                returnKeyType="next"
                 leftIcon={{ type: "material", name: "email" }}
-                style={props.inputStyle} />
+                style={props.inputStyle}
+                errorStyle={{ color: 'red' }}
+                autoCapitalize="none"
+                errorMessage='ENTER A VALID ERROR HERE'
+                onSubmitEditing={_ => onNextField(password)} />
             <Input
                 ref={password}
                 placeholder="Password"
+                textContentType="newPassword"
+                secureTextEntry={true}
+                returnKeyType="next"
                 leftIcon={{ type: "material", name: "lock" }}
-                style={props.inputStyle} />
+                style={props.inputStyle}
+                errorStyle={{ color: 'red' }}
+                autoCapitalize="none"
+                errorMessage='ENTER A VALID ERROR HERE'
+                onSubmitEditing={_ => onNextField(confirmPassword)} />
             <Input
                 ref={confirmPassword}
                 placeholder="Confim password"
+                textContentType="password"
+                secureTextEntry={true}
+                returnKeyType="next"
                 leftIcon={{ type: "material", name: "lock" }}
-                style={props.inputStyle} />
+                style={props.inputStyle}
+                autoCapitalize="none"
+                errorStyle={{ color: 'red' }}
+                errorMessage='ENTER A VALID ERROR HERE'
+                onSubmitEditing={_ => onNextField(name)} />
             <Input
                 placeholder="Name"
-                style={props.inputStyle} />
+                returnKeyType="next"
+                ref={name}
+                style={props.inputStyle}
+                onSubmitEditing={_ => onNextField(lastname)} />
             <Input
                 placeholder="Last name"
+                ref={lastname}
                 style={props.inputStyle} />
             <Button
                 title="Sing up"
